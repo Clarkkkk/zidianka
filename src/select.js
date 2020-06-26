342eb7ca4eefd5b71a030118d88290962492573d
Clark <Clark1729@outlook.com>
2020-06-26 18:57:53 +0800

    Sentance translation. Voice and speaker. Layout improved.

--- src/select.js
+++ src/select.js
@@ -1,22 +1,23 @@
 /* global chrome */
 document.documentElement.addEventListener('mouseup', getSelectedText);
 function getSelectedText(event) {
-  const selectedObj = window.getSelection();
+  let selectedObj = window.getSelection();
+  // if there is already a card
   // remove the card when click elsewhere again
   // and clear the Selection object immediately
-  if (document.getElementById('dict-card') &&
-    event.target != document.getElementById('dict-card') &&
-    event.target.parentElement != document.getElementById('dict-card')) {
-    document.getElementById('dict-card').remove();
-    selectedObj = null;
-  }
-  const selected = selectedObj.toString();
-  if (selected.length > 0) {
-    if (selected.length < 400) {
-      chrome.runtime.sendMessage({selected: selected}, (response) => {
-        const card = cardConstruct(response);
-        positionFigure(event, card, selectedObj);
-      });
+  // else, create a new one
+  if (!event.target.classList.contains('dict-card')) {
+    if (document.getElementById('dict-card')) {
+      document.getElementById('dict-card').remove();
+      selectedObj = null;
+    } else {
+      const selected = selectedObj.toString();
+      if (selected.length > 0 && selected.length < 150) {
+        chrome.runtime.sendMessage({selected: selected}, (response) => {
+          const card = cardConstruct(response);
+          positionFigure(event, card, selectedObj);
+        });
+      }
     }
   }
 }
@@ -30,6 +31,7 @@ function cardConstruct(response) {
   const translation = responseObj.translation;
   const webDef = responseObj.web;
   const basicDef = responseObj.basic;
+  const query = responseObj.query;
 
   // construct the display card
   // since where to put it is not clear yet
@@ -38,28 +40,87 @@ function cardConstruct(response) {
   // and append it to the root element
   const container = document.createElement('div');
   container.id = 'dict-card';
+  container.classList.add('dict-card');
   container.style.visibility = 'hidden';
   document.documentElement.appendChild(container);
 
   // display explains
   if (errorCode == 0) {
     if (isWord) {
+      appendNewElemWithText('span', returnPhrase, container, 'title');
       const basicUSPhonetic = basicDef['us-phonetic'];
       const basicUKPhonetic = basicDef['uk-phonetic'];
+      const UKVoiceURL = basicDef['uk-speech'];
+      const USVoiceURL = basicDef['us-speech'];
       const basicExplains = basicDef['explains'];
-      appendNewElemWithText('span', returnPhrase, container, 'title');
-      const phoneticBox = document.createElement('span');
-      phoneticBox.id = 'phoneticBox';
-      container.appendChild(phoneticBox);
-      appendNewElemWithText('span', 'US /' + basicUSPhonetic + '/', phoneticBox, 'phonetic');
-      appendNewElemWithText('span', 'UK /' + basicUKPhonetic + '/', phoneticBox, 'phonetic');
+      const phoneticLine = document.createElement('span');
+      phoneticLine.id = 'phoneticLine';
+      phoneticLine.classList.add('dict-card');
+      container.appendChild(phoneticLine);
+      phoneticBuild(basicUSPhonetic, USVoiceURL, 'US', phoneticLine);
+      phoneticBuild(basicUKPhonetic, UKVoiceURL, 'UK', phoneticLine);
       for (const entry of basicExplains) {
         appendNewElemWithText('span', entry, container, 'explains');
       }
+    } else {
+      const titleBox = document.createElement('span');
+      titleBox.classList.add('titleBox');
+      container.appendChild(titleBox);
+      appendNewElemWithText('span', query, titleBox, 'title');
+      if (responseObj['speakUrl']) {
+        speakerBuild(responseObj['speakUrl'], titleBox);
+      }
+      appendNewElemWithText('span', translation, container, 'explains');
     }
   }
-
   return container;
+}
+
+// auxiliary function to create the phonetic line
+function phoneticBuild(phonetic, voiceURL, area, container) {
+  if (phonetic) {
+    // display phonetic notations
+    const phoneticBox = document.createElement('span');
+    phoneticBox.classList.add('dict-card', 'phoneticBox');
+    container.appendChild(phoneticBox);
+    appendNewElemWithText('span', area, phoneticBox, 'area');
+    appendNewElemWithText('span', phonetic, phoneticBox, 'phonetic');
+
+    // create the speaker image
+    speakerBuild(voiceURL, phoneticBox);
+  }
+}
+
+function speakerBuild(url, container) {
+  const speaker = document.createElement('img');
+  const speakerURL = chrome.runtime.getURL('src/speaker.png');
+  const speakerPlayingURL = chrome.runtime.getURL('src/speaking.gif');
+  speaker.src = speakerURL;
+  speaker.alt = 'speaker';
+  speaker.classList.add('speaker');
+  speaker.classList.add('dict-card');
+
+  // create the pronounciation voice source
+  const audio = document.createElement('audio');
+  audio.src = url;
+  document.documentElement.appendChild(audio);
+
+  // display an animation while playing
+  audio.addEventListener('play', () => {
+    speaker.src = speakerPlayingURL;
+  });
+  // end the animation when the playback ends
+  audio.addEventListener('ended', () => {
+    speaker.src = speakerURL;
+  });
+
+  // click the speaker to start playing
+  speaker.addEventListener('click', (event) => {
+    audio.play();
+  });
+
+  container.appendChild(speaker);
+  return speaker;
 }
 
 // auxiliary function to create a new element
@@ -70,6 +131,7 @@ function appendNewElemWithText(type, text, parent, className) {
     if (className) {
       elementNode.classList.add(className);
     }
+    elementNode.classList.add('dict-card');
     const textNode = document.createTextNode(text);
     elementNode.appendChild(textNode);
     parent.appendChild(elementNode);
@@ -161,7 +223,7 @@ function positionFigure(event, card, selected) {
 
 
 // get mouse coordinate(for test)
-/* 
+/*
 const ordinate = appendNewElemWithText('div', ' ', document.documentElement);
 ordinate.style.position = 'absolute';
 ordinate.style.zIndex = '1000';
